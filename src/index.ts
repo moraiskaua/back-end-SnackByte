7467d5ad387996729e4e5a558d246530632345c6
moraiskaua <moraiskaua03@gmail.com>
2024-02-16 20:55:41 -0300

    feat: create category model schema

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose
+  .connect('mongodb://localhost:27017/SnackByte')
+  .then(() => {
+    const app = express();
 
-app.listen(8080, () => console.log('🚀 Server is running'));
+    app.listen(8080, () => console.log('🚀 Server is running'));
+  })
+  .catch(() => console.log('❌ Connection error'));
