1e80b8fef666259a17a120e718ef0911d4db9a2c
moraiskaua <moraiskaua03@gmail.com>
2024-02-16 21:25:32 -0300

    feat: create routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { router } from './app/router';
 
 mongoose
   .connect('mongodb://localhost:27017/SnackByte')
   .then(() => {
     const app = express();
+
+    app.use(router);
 
     app.listen(8080, () => console.log('🚀 Server is running'));
   })
