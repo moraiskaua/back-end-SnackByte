400f02a058eb690ee119c6bc02008fcd62b028a1
moraiskaua <moraiskaua03@gmail.com>
2024-02-19 01:12:42 -0300

    fix: remove pusher

--- src/app/useCases/orders/createOrder.ts
+++ src/app/useCases/orders/createOrder.ts
@@ -1,23 +1,12 @@
 import { Request, Response } from 'express';
 import { Order } from '../../models/Order';
-import Pusher from 'pusher';
-
-const pusher = new Pusher({
-  appId: process.env.APP_ID as string,
-  key: process.env.KEY as string,
-  secret: process.env.SECRET as string,
-  cluster: process.env.CLUSTER as string,
-  useTLS: true,
-});
 
 export const createOrder = async (req: Request, res: Response) => {
   try {
     const { table, products } = req.body;
 
     const order = await Order.create({ table, products });
-    const orderDetails = await order.populate('products.product');
 
-    pusher.trigger('snackbyte@websocket', 'orders-new', orderDetails);
     res.status(201).json(order);
   } catch {
     res.sendStatus(500);
